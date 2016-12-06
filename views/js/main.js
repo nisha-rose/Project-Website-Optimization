f8b7a3b0f099480797e83f3f59605abf290c7756
nisha-rose <nisharose.vj@gmail.com>
2016-12-06 16:17:53 +0530

    Update main.js

--- views/js/main.js
+++ views/js/main.js
@@ -449,10 +449,22 @@ var resizePizzas = function(size) {
 
   // Iterates through pizza elements on the page and changes their widths
   function changePizzaSizes(size) {
-    for (var i = 0; i < document.querySelectorAll(".randomPizzaContainer").length; i++) {
-      var dx = determineDx(document.querySelectorAll(".randomPizzaContainer")[i], size);
-      var newwidth = (document.querySelectorAll(".randomPizzaContainer")[i].offsetWidth + dx) + 'px';
-      document.querySelectorAll(".randomPizzaContainer")[i].style.width = newwidth;
+    var newwidth;
+    switch(size){
+       switch(size) {
+        case "1":
+          newwidth = 25;
+        case "2":
+          newwidth = 33.33;
+        case "3":
+          newwidth=50;
+        default:
+          console.log("bug in sizeSwitcher");
+      } 
+    }
+    var randompizzas = document.querySelectorAll(".randomPizzaContainer");
+    for (var i = 0; i < randompizzas.length; i++) {
+      randompizzas[i].style.width = newwidth + %;
     }
   }
 
@@ -502,8 +514,9 @@ function updatePositions() {
   window.performance.mark("mark_start_frame");
 
   var items = document.querySelectorAll('.mover');
+  var btop = document.body.scrollTop / 1250;
   for (var i = 0; i < items.length; i++) {
-    var phase = Math.sin((document.body.scrollTop / 1250) + (i % 5));
+    var phase = Math.sin(btop + (i % 5));
     items[i].style.left = items[i].basicLeft + 100 * phase + 'px';
   }
 
